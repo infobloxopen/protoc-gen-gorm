4773a66c810222e9ef844a8209b41daac10e3abb
Daniel McDonald <dmcdonald@infoblox.com>
2018-04-17 13:38:33 -0700

    add disclaimer

--- types/types.override.pb.go
+++ types/types.override.pb.go
@@ -1,3 +1,4 @@
 package gormable_types
 
+// XXX_WellKnownType is a hack -- please don't make a pattern out of this! This is used to trick certain toolsets to treat this field like a WKT StringValue.
 func (*UUIDValue) XXX_WellKnownType() string { return "StringValue" }
