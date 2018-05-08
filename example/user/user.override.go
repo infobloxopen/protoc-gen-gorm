81dc6d2e002cc1777b0dc2bb8e24b9ceab4c1dc9
chorst <chorst@infoblox.com>
2018-05-08 11:44:38 -0700

    Remove Contacts entirely, add comment to the override hook func

--- example/user/user.override.go
+++ example/user/user.override.go
@@ -2,6 +2,9 @@ package user
 
 import "time"
 
+// AfterToPB implements the posthook interface for the User type. This allows
+// us to customize conversion behavior. In this example, we set the User's Age
+// based on the Birthday, instead of storing it separately in the DB
 func (m *UserORM) AfterToPB(user *User) {
 	user.Age = uint32(time.Now().Sub(m.Birthday).Hours() / 24 / 365)
 }
