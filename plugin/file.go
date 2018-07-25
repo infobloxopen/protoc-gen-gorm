e222729ca249c1a11b018819e5626c9ec5393184
Caleb Horst <calebjh1@gmail.com>
2018-07-25 11:44:37 -0700

    Version (#97)
    * Add comments to include the version of p-g-g and atlas-app-toolkit
    
    * Typo
    
    * Add a little extra padding whitespace

--- plugin/file.go
+++ plugin/file.go
@@ -1,10 +1,15 @@
 package plugin
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/gogo/protobuf/proto"
 	plugin "github.com/gogo/protobuf/protoc-gen-gogo/plugin"
 )
+
+var ProtocGenGormVersion string
+var AtlasAppToolkitVersion string
 
 // CleanFiles will prevent generation of any files where no real code is generated
 func (p *OrmPlugin) CleanFiles(response *plugin.CodeGeneratorResponse) {
@@ -19,5 +24,15 @@ func (p *OrmPlugin) CleanFiles(response *plugin.CodeGeneratorResponse) {
 			}
 		}
 		file.Content = CleanImports(file.Content)
+
+		sections := strings.SplitAfterN(file.GetContent(), "\n", 3)
+		versionString := ""
+		if ProtocGenGormVersion != "" {
+			versionString = fmt.Sprintf("\n// Generated with protoc-gen-gorm version: %s\n", ProtocGenGormVersion)
+		}
+		if AtlasAppToolkitVersion != "" {
+			versionString = fmt.Sprintf("%s// Anticipating compatibility with atlas-app-toolkit version: %s\n", versionString, AtlasAppToolkitVersion)
+		}
+		file.Content = proto.String(fmt.Sprintf("%s%s%s%s", sections[0], sections[1], versionString, sections[2]))
 	}
 }
