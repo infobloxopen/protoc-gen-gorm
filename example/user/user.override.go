cc8b1ba2a964db4e32569ab76fca0b1130cd6136
Tom Hayward <thayward@infoblox.com>
2019-04-25 16:54:32 -0700

    Support NULL timestamps.

--- example/user/user.override.go
+++ example/user/user.override.go
@@ -9,6 +9,6 @@ import (
 // us to customize conversion behavior. In this example, we set the User's Age
 // based on the Birthday, instead of storing it separately in the DB
 func (m *UserORM) AfterToPB(ctx context.Context, user *User) error {
-	user.Age = uint32(time.Now().Sub(m.Birthday).Hours() / 24 / 365)
+	user.Age = uint32(time.Now().Sub(*m.Birthday).Hours() / 24 / 365)
 	return nil
 }
