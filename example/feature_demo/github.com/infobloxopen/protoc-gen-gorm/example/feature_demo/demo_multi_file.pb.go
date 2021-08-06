fc9e60d63dbab2692f1c25b808841bdfb63434f2
binjip978 <binjip978@gmail.com>
2021-08-06 13:54:24 +0300

    work on blog post only

--- example/feature_demo/github.com/infobloxopen/protoc-gen-gorm/example/feature_demo/demo_multi_file.pb.go
+++ example/feature_demo/github.com/infobloxopen/protoc-gen-gorm/example/feature_demo/demo_multi_file.pb.go
@@ -21,115 +21,20 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type BlogPost_Corpus int32
-
-const (
-	BlogPost_UNIVERSAL BlogPost_Corpus = 0
-	BlogPost_WEB       BlogPost_Corpus = 1
-)
-
-// Enum value maps for BlogPost_Corpus.
-var (
-	BlogPost_Corpus_name = map[int32]string{
-		0: "UNIVERSAL",
-		1: "WEB",
-	}
-	BlogPost_Corpus_value = map[string]int32{
-		"UNIVERSAL": 0,
-		"WEB":       1,
-	}
-)
-
-func (x BlogPost_Corpus) Enum() *BlogPost_Corpus {
-	p := new(BlogPost_Corpus)
-	*p = x
-	return p
-}
-
-func (x BlogPost_Corpus) String() string {
-	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
-}
-
-func (BlogPost_Corpus) Descriptor() protoreflect.EnumDescriptor {
-	return file_example_feature_demo_demo_multi_file_proto_enumTypes[0].Descriptor()
-}
-
-func (BlogPost_Corpus) Type() protoreflect.EnumType {
-	return &file_example_feature_demo_demo_multi_file_proto_enumTypes[0]
-}
-
-func (x BlogPost_Corpus) Number() protoreflect.EnumNumber {
-	return protoreflect.EnumNumber(x)
-}
-
-// Deprecated: Use BlogPost_Corpus.Descriptor instead.
-func (BlogPost_Corpus) EnumDescriptor() ([]byte, []int) {
-	return file_example_feature_demo_demo_multi_file_proto_rawDescGZIP(), []int{1, 0}
-}
-
-type ExternalChild struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
-}
-
-func (x *ExternalChild) Reset() {
-	*x = ExternalChild{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_example_feature_demo_demo_multi_file_proto_msgTypes[0]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *ExternalChild) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*ExternalChild) ProtoMessage() {}
-
-func (x *ExternalChild) ProtoReflect() protoreflect.Message {
-	mi := &file_example_feature_demo_demo_multi_file_proto_msgTypes[0]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use ExternalChild.ProtoReflect.Descriptor instead.
-func (*ExternalChild) Descriptor() ([]byte, []int) {
-	return file_example_feature_demo_demo_multi_file_proto_rawDescGZIP(), []int{0}
-}
-
-func (x *ExternalChild) GetId() string {
-	if x != nil {
-		return x.Id
-	}
-	return ""
-}
-
 type BlogPost struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Id     uint64          `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
-	Title  string          `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
-	Author string          `protobuf:"bytes,3,opt,name=author,proto3" json:"author,omitempty"`
-	Magic  string          `protobuf:"bytes,4,opt,name=magic,proto3" json:"magic,omitempty"`
-	Corpus BlogPost_Corpus `protobuf:"varint,5,opt,name=corpus,proto3,enum=example.BlogPost_Corpus" json:"corpus,omitempty"`
+	Id     uint64 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Title  string `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
+	Author string `protobuf:"bytes,3,opt,name=author,proto3" json:"author,omitempty"`
 }
 
 func (x *BlogPost) Reset() {
 	*x = BlogPost{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_example_feature_demo_demo_multi_file_proto_msgTypes[1]
+		mi := &file_example_feature_demo_demo_multi_file_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -142,7 +47,7 @@ func (x *BlogPost) String() string {
 func (*BlogPost) ProtoMessage() {}
 
 func (x *BlogPost) ProtoReflect() protoreflect.Message {
-	mi := &file_example_feature_demo_demo_multi_file_proto_msgTypes[1]
+	mi := &file_example_feature_demo_demo_multi_file_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -155,7 +60,7 @@ func (x *BlogPost) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use BlogPost.ProtoReflect.Descriptor instead.
 func (*BlogPost) Descriptor() ([]byte, []int) {
-	return file_example_feature_demo_demo_multi_file_proto_rawDescGZIP(), []int{1}
+	return file_example_feature_demo_demo_multi_file_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *BlogPost) GetId() uint64 {
@@ -179,20 +84,6 @@ func (x *BlogPost) GetAuthor() string {
 	return ""
 }
 
-func (x *BlogPost) GetMagic() string {
-	if x != nil {
-		return x.Magic
-	}
-	return ""
-}
-
-func (x *BlogPost) GetCorpus() BlogPost_Corpus {
-	if x != nil {
-		return x.Corpus
-	}
-	return BlogPost_UNIVERSAL
-}
-
 var File_example_feature_demo_demo_multi_file_proto protoreflect.FileDescriptor
 
 var file_example_feature_demo_demo_multi_file_proto_rawDesc = []byte{
@@ -200,27 +91,17 @@ var file_example_feature_demo_demo_multi_file_proto_rawDesc = []byte{
 	0x65, 0x5f, 0x64, 0x65, 0x6d, 0x6f, 0x2f, 0x64, 0x65, 0x6d, 0x6f, 0x5f, 0x6d, 0x75, 0x6c, 0x74,
 	0x69, 0x5f, 0x66, 0x69, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x07, 0x65, 0x78,
 	0x61, 0x6d, 0x70, 0x6c, 0x65, 0x1a, 0x12, 0x6f, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x2f, 0x67,
-	0x6f, 0x72, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x27, 0x0a, 0x0d, 0x45, 0x78, 0x74,
-	0x65, 0x72, 0x6e, 0x61, 0x6c, 0x43, 0x68, 0x69, 0x6c, 0x64, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64,
-	0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x3a, 0x06, 0xba, 0xb9, 0x19, 0x02,
-	0x08, 0x01, 0x22, 0xc2, 0x01, 0x0a, 0x08, 0x42, 0x6c, 0x6f, 0x67, 0x50, 0x6f, 0x73, 0x74, 0x12,
-	0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04, 0x52, 0x02, 0x69, 0x64, 0x12,
-	0x14, 0x0a, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05,
-	0x74, 0x69, 0x74, 0x6c, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x18,
-	0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x12, 0x1c, 0x0a,
-	0x05, 0x6d, 0x61, 0x67, 0x69, 0x63, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x42, 0x06, 0xba, 0xb9,
-	0x19, 0x02, 0x10, 0x01, 0x52, 0x05, 0x6d, 0x61, 0x67, 0x69, 0x63, 0x12, 0x30, 0x0a, 0x06, 0x63,
-	0x6f, 0x72, 0x70, 0x75, 0x73, 0x18, 0x05, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x18, 0x2e, 0x65, 0x78,
-	0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2e, 0x42, 0x6c, 0x6f, 0x67, 0x50, 0x6f, 0x73, 0x74, 0x2e, 0x43,
-	0x6f, 0x72, 0x70, 0x75, 0x73, 0x52, 0x06, 0x63, 0x6f, 0x72, 0x70, 0x75, 0x73, 0x22, 0x20, 0x0a,
-	0x06, 0x43, 0x6f, 0x72, 0x70, 0x75, 0x73, 0x12, 0x0d, 0x0a, 0x09, 0x55, 0x4e, 0x49, 0x56, 0x45,
-	0x52, 0x53, 0x41, 0x4c, 0x10, 0x00, 0x12, 0x07, 0x0a, 0x03, 0x57, 0x45, 0x42, 0x10, 0x01, 0x3a,
-	0x06, 0xba, 0xb9, 0x19, 0x02, 0x08, 0x01, 0x42, 0x46, 0x5a, 0x44, 0x67, 0x69, 0x74, 0x68, 0x75,
-	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x6e, 0x66, 0x6f, 0x62, 0x6c, 0x6f, 0x78, 0x6f, 0x70,
-	0x65, 0x6e, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x2d, 0x67, 0x65, 0x6e, 0x2d, 0x67, 0x6f,
-	0x72, 0x6d, 0x2f, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2f, 0x66, 0x65, 0x61, 0x74, 0x75,
-	0x72, 0x65, 0x5f, 0x64, 0x65, 0x6d, 0x6f, 0x3b, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x6f, 0x72, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x50, 0x0a, 0x08, 0x42, 0x6c, 0x6f,
+	0x67, 0x50, 0x6f, 0x73, 0x74, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28,
+	0x04, 0x52, 0x02, 0x69, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x18, 0x02,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x61,
+	0x75, 0x74, 0x68, 0x6f, 0x72, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x61, 0x75, 0x74,
+	0x68, 0x6f, 0x72, 0x3a, 0x06, 0xba, 0xb9, 0x19, 0x02, 0x08, 0x01, 0x42, 0x46, 0x5a, 0x44, 0x67,
+	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x6e, 0x66, 0x6f, 0x62, 0x6c,
+	0x6f, 0x78, 0x6f, 0x70, 0x65, 0x6e, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x2d, 0x67, 0x65,
+	0x6e, 0x2d, 0x67, 0x6f, 0x72, 0x6d, 0x2f, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2f, 0x66,
+	0x65, 0x61, 0x74, 0x75, 0x72, 0x65, 0x5f, 0x64, 0x65, 0x6d, 0x6f, 0x3b, 0x65, 0x78, 0x61, 0x6d,
+	0x70, 0x6c, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -235,20 +116,16 @@ func file_example_feature_demo_demo_multi_file_proto_rawDescGZIP() []byte {
 	return file_example_feature_demo_demo_multi_file_proto_rawDescData
 }
 
-var file_example_feature_demo_demo_multi_file_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
-var file_example_feature_demo_demo_multi_file_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_example_feature_demo_demo_multi_file_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_example_feature_demo_demo_multi_file_proto_goTypes = []interface{}{
-	(BlogPost_Corpus)(0),  // 0: example.BlogPost.Corpus
-	(*ExternalChild)(nil), // 1: example.ExternalChild
-	(*BlogPost)(nil),      // 2: example.BlogPost
+	(*BlogPost)(nil), // 0: example.BlogPost
 }
 var file_example_feature_demo_demo_multi_file_proto_depIdxs = []int32{
-	0, // 0: example.BlogPost.corpus:type_name -> example.BlogPost.Corpus
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
 }
 
 func init() { file_example_feature_demo_demo_multi_file_proto_init() }
@@ -258,18 +135,6 @@ func file_example_feature_demo_demo_multi_file_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_example_feature_demo_demo_multi_file_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ExternalChild); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_example_feature_demo_demo_multi_file_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*BlogPost); i {
 			case 0:
 				return &v.state
@@ -287,14 +152,13 @@ func file_example_feature_demo_demo_multi_file_proto_init() {
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_example_feature_demo_demo_multi_file_proto_rawDesc,
-			NumEnums:      1,
-			NumMessages:   2,
+			NumEnums:      0,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_example_feature_demo_demo_multi_file_proto_goTypes,
 		DependencyIndexes: file_example_feature_demo_demo_multi_file_proto_depIdxs,
-		EnumInfos:         file_example_feature_demo_demo_multi_file_proto_enumTypes,
 		MessageInfos:      file_example_feature_demo_demo_multi_file_proto_msgTypes,
 	}.Build()
 	File_example_feature_demo_demo_multi_file_proto = out.File
